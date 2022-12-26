7a3a76d7e474079b685183076d83fa4bc0b5765f
gabeFrancisco <gabegit2@gmail.com>
2022-12-25 22:38:10 -0200

    Added grainy gradient and box shadow to the Header component

--- BielDev.Web/ClientApp/src/components/Header.js
+++ BielDev.Web/ClientApp/src/components/Header.js
@@ -1,26 +1,27 @@
 import React from "react";
 
-import './Header.css'
+import "./Header.css";
 
-import Avatar from '../assets/avatar.jpg'
+import Avatar from "../assets/avatar.jpg";
 // import Mountain from '../assets/mountain.jpg'
 
 export default function Header() {
-    return (
-        <header>
-            <div className="Logo-Area">
-               <img src={Avatar} id="Avatar" alt="Avatar"/>
-            </div>
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Tecnologias</li>
-                    <li>Sobre mim</li>
-                </ul>
-            </div>
-            {/* <div className="Mountain-Area">
+  return (
+    <header>
+      <div className="Logo-Area">
+        <img src={Avatar} id="Avatar" alt="Avatar" />
+        <h3>Gabriel Francisco</h3>
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Tecnologias</li>
+          <li>Sobre mim</li>
+        </ul>
+      </div>
+      {/* <div className="Mountain-Area">
                 <img src={Mountain} id="Mountain" alt="Mountain"/>
             </div> */}
-        </header>
-    )
+    </header>
+  );
 }
